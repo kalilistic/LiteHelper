f3ce76b42549aeae029e60d7cf8589defbf0dbb0
kalilistic <35899782+kalilistic@users.noreply.github.com>
2023-04-23 19:58:25 -0400

    fix: remove null bugs from ext methods

--- LiteHelper/Extensions/BsonDocumentExtensions.cs
+++ LiteHelper/Extensions/BsonDocumentExtensions.cs
@@ -10,15 +10,14 @@ namespace LiteHelper.Extensions;
 public static class BsonDocumentExtensions
 {
     /// <summary>
-    /// Gets the value of the specified key in the BsonDocument, or returns the default value if the key is not found or the value cannot be cast to the requested type.
+    /// Gets the value associated with the specified key or the default value if the key is not found or the value is null.
     /// </summary>
-    /// <typeparam name="T">The type of the value to be returned.</typeparam>
-    /// <param name="bsonDocument">The BsonDocument to retrieve the value from.</param>
-    /// <param name="key">The key of the value to retrieve.</param>
-    /// <param name="defaultValue">The default value to return if the key is not found or the value cannot be cast to the requested type. Default is the default value of the type T.</param>
-    /// <returns>The value of the specified key if found and can be cast to the requested type, or the default value otherwise.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when the bsonDocument parameter is null.</exception>
-    public static T? GetValueOrDefault<T>(this BsonDocument bsonDocument, string key, T? defaultValue = default)
+    /// <typeparam name="T">The type of the value to get.</typeparam>
+    /// <param name="bsonDocument">The BsonDocument to get the value from.</param>
+    /// <param name="key">The key of the value to get.</param>
+    /// <param name="defaultValue">The default value to return if the key is not found or the value is null.</param>
+    /// <returns>The value associated with the specified key or the default value if the key is not found or the value is null.</returns>
+    public static T GetValueOrDefault<T>(this BsonDocument bsonDocument, string key, T defaultValue = default!)
     {
         if (bsonDocument == null)
         {
@@ -29,7 +28,11 @@ public static class BsonDocumentExtensions
         {
             try
             {
-                return (T)Convert.ChangeType(bsonValue.RawValue, typeof(T));
+                var convertedValue = Convert.ChangeType(bsonValue.RawValue, Nullable.GetUnderlyingType(typeof(T)) ?? typeof(T));
+                if (convertedValue is T value)
+                {
+                    return value;
+                }
             }
             catch (InvalidCastException)
             {
