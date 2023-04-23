504c8c7e9aae840a175560720a0eea1537bbc55c
kalilistic <35899782+kalilistic@users.noreply.github.com>
2023-04-23 17:46:01 -0400

    feat: add ValidateKeyHasValue ext method

--- LiteHelper/Extensions/BsonDocumentExtensions.cs
+++ LiteHelper/Extensions/BsonDocumentExtensions.cs
@@ -39,4 +39,29 @@ public static class BsonDocumentExtensions
 
         return defaultValue;
     }
+
+    /// <summary>
+    /// Ensures that the specified key exists in the BsonDocument and its value is not null or empty.
+    /// </summary>
+    /// <param name="bsonDocument">The BsonDocument to check for the key.</param>
+    /// <param name="key">The key of the value to check.</param>
+    /// <exception cref="ArgumentNullException">Thrown when the bsonDocument parameter is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when the key is not found in the bsonDocument or its value is null or empty.</exception>
+    public static void ValidateKeyHasValue(this BsonDocument bsonDocument, string key)
+    {
+        if (bsonDocument == null)
+        {
+            throw new ArgumentNullException(nameof(bsonDocument));
+        }
+
+        if (!bsonDocument.TryGetValue(key, out var bsonValue))
+        {
+            throw new ArgumentException($"Key '{key}' not found in the BsonDocument.", nameof(key));
+        }
+
+        if (bsonValue.IsNull || (bsonValue.IsString && string.IsNullOrEmpty(bsonValue.AsString)))
+        {
+            throw new ArgumentException($"The value for the key '{key}' is null or empty.", nameof(key));
+        }
+    }
 }
